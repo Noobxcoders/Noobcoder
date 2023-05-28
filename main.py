f8d5995c03f7354335a7e5d2c0654a432071207d
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-05-28 21:32:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from cache.main import call_py, bot
+from anime.main import call_py, bot
 
 async def start_bot():
     print("[INFO]: sᴛᴀʀᴛɪɴɢ noobcoder ʙᴏᴛ ᴄʟɪᴇɴᴛ")
